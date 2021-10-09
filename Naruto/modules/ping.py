c060038ac24db38b87a6658a8ee94b712774ef67
Jass Manak <82396523+JassManak1125@users.noreply.github.com>
2021-10-09 12:28:41 +0530

    Update ping.py

--- Naruto/modules/ping.py
+++ Naruto/modules/ping.py
@@ -5,8 +5,8 @@ import requests
 from telegram import Bot, Update, ParseMode
 from telegram.ext import run_async
 
-from tg_bot import dispatcher, StartTime
-from tg_bot.modules.disable import DisableAbleCommandHandler
+from Naruto import dispatcher, StartTime
+from Naruto.modules.disable import DisableAbleCommandHandler
 
 sites_list = {
     "Telegram": "https://api.telegram.org",
@@ -72,10 +72,10 @@ def ping(bot: Bot, update: Update):
     telegram_ping = ping_func(["Telegram"])[0].split(": ", 1)[1]
     uptime = get_readable_time((time.time() - StartTime))
 
-    reply_msg = ("PONG!!\n"
-                 "<b>Time Taken:</b> <code>{}</code>\n"
-                 "<b>Service uptime:</b> <code>{}</code>".format(telegram_ping, uptime))
-
+    reply_msg = ("ᴘɪɴɢ ᴘᴏɴɢ ᴅɪɴɢ ᴅᴏɴɢ..🤪\n\n"
+                 "<b>ᴛɪᴍᴇ ᴛᴀᴋᴇɴ »»</b> <code>{}</code>\n"
+                 "<b>ᴏɴ sᴇʀᴠɪᴄᴇ »»</b> <code>{}</code>".format(telegram_ping, uptime))
+    
     update.effective_message.reply_text(reply_msg, parse_mode=ParseMode.HTML)
 
 
