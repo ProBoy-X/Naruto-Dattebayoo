76e8d56df9c4ec74aa096d92f29695be6200307c
MadBoy <73926361+madboy482@users.noreply.github.com>
2021-10-14 17:39:36 +0530

    k

--- Naruto/modules/admin.py
+++ Naruto/modules/admin.py
@@ -121,9 +121,7 @@ def demote(bot: Bot, update: Update, args: List[str]) -> str:
                               can_pin_messages=False,
                               can_promote_members=False)
         message.reply_text(tld(chat.id, f"ᴅᴇᴍᴏᴛᴇ ʜᴏɢʏᴇ in *{chatD.title}*!"), parse_mode=ParseMode.MARKDOWN)
-        return f"<b>{html.escape(chatD.title)}:</b> ʙʜᴀɪʏᴀ.." 
-               f"\n<b>Admin:</b> {mention_html(user.id, user.first_name)}" 
-               f"\n<b>User:</b> {mention_html(user_member.user.id, user_member.user.first_name)}"
+        return f"<b>{html.escape(chatD.title)}:</b> ʙʜᴀɪʏᴀ..\n<b>Admin:</b> {mention_html(user.id, user.first_name)}\n\n<b>User:</b> {mention_html(user_member.user.id, user_member.user.first_name)}"
 
     except BadRequest:
         message.reply_text(
